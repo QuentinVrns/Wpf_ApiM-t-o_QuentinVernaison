ff54aa1d767bb5afcf22ee3bc373eeab16ba38f2
quentin vernaison <quentin.vernaison@saintmichelannecy.fr>
2023-12-01 01:37:18 +0100

    v2.5

--- Wpf_ApiMétéo_QuentinVernaison/MainWindow.xaml.cs
+++ Wpf_ApiMétéo_QuentinVernaison/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.IO;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 namespace Wpf_ApiMétéo_QuentinVernaison
 {
@@ -851,62 +855,145 @@ namespace Wpf_ApiMétéo_QuentinVernaison
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ImageSource _temperatureImage;
+        
+        private WeatherService weatherService = new WeatherService();
+        private string baseUrl = "https://www.prevision-meteo.ch/services/json/";
+        
         public MainWindow()
         {
             InitializeComponent();
-        _=  GetWeather();
+            InitializeComboBox();
+
+            //Appeler la méthode Timer
+            Timer();
+            // Sélectionner Annecy par défaut lors du chargement de la fenêtre
+            Cb_Ville.SelectedItem = "Annecy";
+            // Appeler la méthode GetWeather avec la ville sélectionnée
+            _ = GetWeather("Annecy");
+            //si la personne selectionne une autre ville dans la combobox afficher la météo de cette ville
+            Cb_Ville.SelectionChanged += Cb_Ville_SelectionChanged;
+
+            
+
+
         }
 
-        public async Task<string> GetWeather()
+
+        //Méthode qui permet d'initialiser la combobox
+        private void InitializeComboBox()
+        {
+            // Associer la liste des villes à la ComboBox
+            Cb_Ville.ItemsSource = weatherService.GetCityList();
+        }
+
+        //Méthode qui permet de changer la météo en fonction de la ville sélectionnée
+        private async void Cb_Ville_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Obtenir la ville sélectionnée
+            string selectedCity = Cb_Ville.SelectedItem as string;
+
+            // Appeler la méthode GetWeather avec la ville sélectionnée
+            await GetWeather(selectedCity);
+        }
+
+        private void Btn_Ajouter_Click(object sender, RoutedEventArgs e)
+        {
+            // Ajouter une ville dans la ComboBox
+            if (Cb_Ville.Items.Contains(Tb_Ajouter_Ville.Text))
+            {
+                MessageBox.Show("Ville deja ajoutée");
+            }
+            else
+            {
+                //Ajoute la ville dans la combobox
+                Cb_Ville.Items.Add(Tb_Ajouter_Ville.Text);
+                Cb_Ville.SelectedItem = Tb_Ajouter_Ville.Text;
+
+
+
+                Tb_Ajouter_Ville.Text = "";
+            }
+        }
+
+        private void Timer()
+        {
+            // Faire un timer qui s'actualise toutes les 10 minutes pour actualiser la météo
+            DispatcherTimer timer = new DispatcherTimer();           
+            timer.Interval = TimeSpan.FromMinutes(10);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+        }
+
+        //Méthode qui s'actualise toutes les 10 minutes
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Actualiser la météo toutes les 10 secondes
+            // Appeler la méthode GetWeather avec la ville sélectionnée
+            _= GetWeather(Cb_Ville.SelectedItem as string);
+            
+        }
+        
+        // Permet d'afficher la météo de la ville sélectionnée
+        public async Task<string> GetWeather(string city)
         {
             HttpClient client = new HttpClient();
             try
             {
-                HttpResponseMessage response = await client.GetAsync("https://www.prevision-meteo.ch/services/json/Annecy");
+                string apiUrl = $"{baseUrl}{city}";
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
                 if (response.IsSuccessStatusCode)
                 {
                     var content = await response.Content.ReadAsStringAsync();
                     Root root = JsonConvert.DeserializeObject<Root>(content);
 
+                   
                     CurrentCondition currentcondition = root.current_condition;
                     FcstDay0 fcstDay0 = root.fcst_day_0;
+                    FcstDay1 fcstDay1 = root.fcst_day_1;
+                    FcstDay2 fcstDay2 = root.fcst_day_2;
+                    FcstDay3 fcstDay3 = root.fcst_day_3;
+                    HourlyData hourlyData = root.fcst_day_0.hourly_data;
 
-                    Tb_Temperature.Text = currentcondition.tmp.ToString() + "°C";
-                    Tb_Humidite.Text = currentcondition.humidity.ToString() + "%";
-                    Tb_Vent.Text = currentcondition.wnd_spd.ToString() + "km/h";
-                    Tb_Precipitation.Text = currentcondition.pressure.ToString() + "hPa";
-                    Tb_Plus.Text = fcstDay0.tmax.ToString() + "°C";
-                    Tb_Moins.Text = fcstDay0.tmin.ToString() + "°C";
-                    Tb_Date.Text = fcstDay0.date.ToString();
-                    Tb_Jour1Date.Text = root.fcst_day_1.date.ToString();
-                    Tb_Jour2Date.Text = root.fcst_day_2.date.ToString();
-                    Tb_Jour3Date.Text = root.fcst_day_3.date.ToString();
+                    //Afficher les images
+                    ImageTemperature.Source = new BitmapImage(new Uri(currentcondition.icon_big));
+                    ImageTemperatureUn.Source = new BitmapImage(new Uri(fcstDay0.icon_big));
+                    ImageTemperatureDeux.Source = new BitmapImage(new Uri(fcstDay1.icon_big));
+                    ImageTemperatureTrois.Source = new BitmapImage(new Uri(fcstDay2.icon_big));
 
-                    Tb_HeurePlusUn.Text = fcstDay0.hourly_data._1H00.TMP2m.ToString() + "°C";
-                    Tb_HeurePlusDeux.Text = fcstDay0.hourly_data._2H00.TMP2m.ToString() + "°C";
-                    Tb_HeurePlusTrois.Text = fcstDay0.hourly_data._3H00.TMP2m.ToString() + "°C";
-                    Tb_HeurePlusQuatre.Text = fcstDay0.hourly_data._4H00.TMP2m.ToString() + "°C";
-                    Tb_HeurePlusCinq.Text = fcstDay0.hourly_data._5H00.TMP2m.ToString() + "°C";
-                    Tb_MoinsJour1.Text = root.fcst_day_1.tmin.ToString() + "°C";
-                    Tb_PlusJour1.Text = root.fcst_day_1.tmax.ToString() + "°C";
-                    Tb_MoinsJour2.Text = root.fcst_day_2.tmin.ToString() + "°C";
-                    Tb_PlusJour2.Text = root.fcst_day_2.tmax.ToString() + "°C";
-                    Tb_MoinsJour3.Text = root.fcst_day_3.tmin.ToString() + "°C";
-                    Tb_PlusJour3.Text = root.fcst_day_3.tmax.ToString() + "°C";
+                    //Afficher la température actuelle
+                    Tb_Temperature.Text = currentcondition.tmp + "°C";
 
+                    //Afficher le vent, la pression et l'humidité 
+                    Tb_Humidite.Text = "Humidité" + ": " + currentcondition.humidity + "%";
+                    Tb_Vent.Text = "Vent" + ": " + currentcondition.wnd_spd + "km/h";
+                    Tb_Precipitation.Text = "Pression" + ": " + currentcondition.pressure + "hPa";
 
+                    //Afficher les températures plus et moins du jour
+                    Tb_Plus.Text = fcstDay0.tmax + "°C";
+                    Tb_Moins.Text = fcstDay0.tmin + "°C";
 
+                    //Afficher la date du jour
+                    Tb_Date.Text = fcstDay0.day_long + "  " + fcstDay0.date;
 
-                    _temperatureImage = new BitmapImage(new Uri(currentcondition.icon_big));
-                    ImageTemperature.Source = _temperatureImage;
+                    //Afficher les dates des jours suivants
+                    Tb_Jour1Date.Text = fcstDay1.day_short + "  " + fcstDay1.date;
+                    Tb_Jour2Date.Text = fcstDay2.day_short + "  " + fcstDay2.date;
+                    Tb_Jour3Date.Text = fcstDay3.day_short + "  " + fcstDay3.date;
 
+                    //Afficher les températures plus et moins des jours suivants
+                    Tb_MoinsJour1.Text = fcstDay1.tmin + "°C";
+                    Tb_PlusJour1.Text = fcstDay1.tmax + "°C";
+                    Tb_MoinsJour2.Text = fcstDay2.tmin + "°C";
+                    Tb_PlusJour2.Text = fcstDay2.tmax + "°C";
+                    Tb_MoinsJour3.Text = fcstDay3.tmin + "°C";
+                    Tb_PlusJour3.Text = fcstDay3.tmax + "°C";
 
+                    //Afficher la température à 00h00 , 08h00 ,12h00 , 16h00 et 20h00
 
-
-
-
-
+                    
+                    
 
                     return "";
                 }
@@ -916,13 +1003,25 @@ namespace Wpf_ApiMétéo_QuentinVernaison
                 return ex.Message;
             }
 
-
             return null;
+        }
+    }
 
-
+    //Classe qui permet de récupérer les données de la météo
+    public class WeatherService
+    {
+        public List<string> GetCityList()
+        {
+            // Retourner la liste des villes, par exemple, Annecy, Paris, etc.
+            return new List<string> { "Annecy", "Paris", "Lyon", "Marseille", "Grenoble", "Lille" };
         }
 
-        
+
     }
+
+    
 }
+
+
+
 
